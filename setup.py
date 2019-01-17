f652ade886214ecb74beeb39f6fb5533e03305ed
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-17 18:27:19 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+__author__ = 'Lucifer Avada'
+
+from setuptools import setup
+
+setup(
+    name='sugar-asynctest',
+    version='0.0.1',
+    author='Lucifer Avada',
+    author_email='lucifer.avada@gmail.com',
+    url='https://github.com/sugarush/sugar-asynctest',
+    packages=['sugar_asynctest'],
+    description='An async testcase for the unittest module.',
+    install_requires=[ ]
+)
